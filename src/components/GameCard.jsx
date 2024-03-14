73907d593fbf46f02704d319aa53de02a3c8196c
Hemyulin <daniel.budiansky@protonmail.com>
2024-03-14 16:21:35 +0100

    card design

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -4,7 +4,10 @@ export const GameCard = ({ card }) => {
   return (
     <div className="game-card">
       <img src={card.screenshots[0]} />
-      <h3>{card.devName}</h3>
+      <h3>{card.gameName}</h3>
+      <h4>{card.devName}</h4>
+      <p>{card.description}</p>
+      <p>{card.price}€</p>
     </div>
   );
 };
