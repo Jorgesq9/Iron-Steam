a3370bc311e55fb18f52d5f72bd70c05299fe539
Hemyulin <daniel.budiansky@protonmail.com>
2024-03-14 15:32:53 +0100

    working on API request to localhost

--- src/components/GameList.jsx
+++ src/components/GameList.jsx
@@ -1,19 +1,24 @@
+import { useEffect, useState } from "react";
 import { GameCard } from "./GameCard";
+import axios from "axios";
 
 export const GameList = () => {
+  const [cards, setCards] = useState();
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get("http://localhost:5001/games");
+        setCards(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getData();
+  }, []);
+
   return (
     <div className="games-grid">
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
-      <GameCard />
       <GameCard />
     </div>
   );
