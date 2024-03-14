db17cd331bd32ece550df2c1af29bd0fdd3482b3
Hemyulin <daniel.budiansky@protonmail.com>
2024-03-14 15:20:10 +0100

    added seperate components for game cards

---
+++ src/components/GameCard.jsx
@@ -0,0 +1,5 @@
+import "./GameCard.css";
+
+export const GameCard = () => {
+  return <div className="game-card">GameCard</div>;
+};
---
+++ src/components/GameList.jsx
@@ -0,0 +1,20 @@
+import { GameCard } from "./GameCard";
+
+export const GameList = () => {
+  return (
+    <div className="games-grid">
+      <GameCard />
+      <GameCard />
+      <GameCard />
+      <GameCard />
+      <GameCard />
+      <GameCard />
+      <GameCard />
+      <GameCard />
+      <GameCard />
+      <GameCard />
+      <GameCard />
+      <GameCard />
+    </div>
+  );
+};
