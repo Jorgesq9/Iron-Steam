ee4fd7c13fb226d6db8a850a1a55efbf45040997
Hemyulin <daniel.budiansky@protonmail.com>
2024-03-14 13:28:56 +0100

    merge conflicts resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import About from "./Pages/About";
-import "./App.css";
 import { HomePage } from "./pages/HomePage";
+import { AboutPage } from "./pages/AboutPage";
+import { GameDetailsPage } from "./pages/GameDetails";
+import { NotFoundPage } from "./pages/NotFoundPage";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="gameDetails" element={<GameDetails />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="gameDetails" element={<GameDetailsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
   );
