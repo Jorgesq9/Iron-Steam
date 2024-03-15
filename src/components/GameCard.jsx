c2e3ef1a526247eb9935d9fc14864a91bfe1142c
Jorge Esquiva <jorgeesquivait@gmail.com>
2024-03-15 10:18:35 +0100

    gamecard

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -4,8 +4,6 @@ export const GameCard = ({ card }) => {
     <div className="game-card card">
       <img src={card.screenshots[0]} />
       <h3>{card.gameName}</h3>
-      <h4>{card.devName}</h4>
-      <p>{card.description}</p>
       <p>{card.price}€</p>
     </div>
   );
