43ceb2b1a6ae08934ccc6bdebc07f33f201c9da8
Hemyulin <daniel.budiansky@protonmail.com>
2024-03-15 10:06:19 +0100

    dark mode and some styling

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,12 +1,12 @@
 import "./GameCard.css";
 export const GameCard = ({ card }) => {
   return (
-    <div className="game-card">
+    <div className="game-card card">
       <img src={card.screenshots[0]} />
       <h3>{card.gameName}</h3>
       <h4>{card.devName}</h4>
       <p>{card.description}</p>
       <p>{card.price}€</p>
-   </div>
+    </div>
   );
 };
