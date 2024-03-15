f4277b2568566444e789d6af882f21bce6d0af09
Jorge Esquiva <jorgeesquivait@gmail.com>
2024-03-15 09:50:39 +0100

    detailsPage

--- src/Pages/GameDetails.jsx
+++ src/Pages/GameDetails.jsx
@@ -1,5 +1,48 @@
 import "./GameDetails.css";
+import { useEffect, useState } from "react";
+import axios from 'axios'
 
 export const GameDetailsPage = () => {
-  return <div className="game-details-page">GameDetailsPage</div>;
+
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get("http://localhost:5001/games");
+        setCards(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getData();
+  }, []);
+
+
+  return(
+     <h1 className="game-details-page">GameDetailsPage
+     
+          <div>
+
+          <ul>
+
+          {cards.map((card) => {
+            return (
+                 <div className="game-card">
+                 <img src={card.screenshots[0]} />
+                 <h3>{card.gameName}</h3>
+                 <h4>{card.devName}</h4>
+                 <p>{card.description}</p>
+                 <p>{card.price}€</p>
+              </div>
+            )
+          })}
+
+          </ul>
+
+          </div>
+     
+     </h1>
+  
+     )
 };
