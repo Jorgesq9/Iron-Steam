80919457360e80c449e9d6a171cbdbc490b85c46
Jorge Esquiva <jorgeesquivait@gmail.com>
2024-03-20 13:04:40 +0100

    register/login

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,11 +2,15 @@ import { AddGameForm } from "../components/AddGameForm";
 import { GameList } from "../components/GameList";
 import { useEffect, useState } from "react";
 import { Searchbar } from "../components/Searchbar";
+import { UserRegister} from "../components/UserRegister"
+import { UserLogin} from "../components/UserLogin"
 import axios from "axios";
+import "./HomePage.css"
 
 export const HomePage = ({ toggleModal, isModalOpen }) => {
   const [cards, setCards] = useState([]);
   const [search, setSearch] = useState("");
+  const [userIsLogin, setUserIsLogin] = useState(false)
 
   useEffect(() => {
     const getData = async () => {
@@ -24,15 +28,28 @@ export const HomePage = ({ toggleModal, isModalOpen }) => {
     card.gameName.toLowerCase().includes(search.toLowerCase())
   );
 
+  let addButton = null;
+
+  if(userIsLogin) {
+    addButton = ( <button className="button modal-button" onClick={toggleModal}>
+    <h1>Add Game</h1>
+  </button>
+)
+  }
+
   return (
     <div className="home-page">
       <Searchbar search={search} setSearch={setSearch} />
 
-      <AddGameForm toggleModal={toggleModal} isModalOpen={isModalOpen} />
+      {userIsLogin && (
+      <AddGameForm toggleModal={toggleModal} isModalOpen={isModalOpen} userIsLogin={userIsLogin} /> 
+      )}
 
-      <button className="button modal-button" onClick={toggleModal}>
-        <h1>Add Game</h1>
-      </button>
+      
+      {addButton}
+
+      <UserRegister />
+      <UserLogin setUserIsLogin={setUserIsLogin} />
 
       <GameList
         cards={filteredCards}
