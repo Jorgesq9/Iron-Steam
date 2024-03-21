4788b97ee610f1754783c70901152e613f3261d0
Jorge Esquiva <jorgeesquivait@gmail.com>
2024-03-21 21:58:24 +0100

    notFound

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function App() {
             />
           }
         />
-        <Route path="/*" element={<NotFoundPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
 
       <div className="footer">
